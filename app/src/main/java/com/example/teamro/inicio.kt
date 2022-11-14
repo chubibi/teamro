b38eeace199b32d870b092e35bcffb6dffae9465
Josue Isaias Cazares Perez <josue.cazares.10@gmail.com>
2022-11-13 22:57:44 -0600

    Welcome to proyect

---
+++ app/src/main/java/com/example/teamro/inicio.kt
@@ -0,0 +1,11 @@
+package com.example.teamro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class inicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_inicio)
+    }
+}
